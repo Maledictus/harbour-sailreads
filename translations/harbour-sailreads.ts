10f5de28657dd0fe3ff662c946ce68f8a8d9b050
Oleg Linkin <MaledictusDeMagog@gmail.com>
2018-02-18 03:21:33 +0300

    Add base files: images, deploy, translations

---
+++ translations/harbour-sailreads.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AuthorizationPage</name>
+    <message>
+        <source>Authorization failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Authorization process...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
