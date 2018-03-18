2c1e9667c3f6c1bc20444526537f21fca41b1da2
Oleg Linkin <MaledictusDeMagog@gmail.com>
2018-03-18 12:46:06 +0300

    Add lj-like date generator

--- qml/utils/Utils.js
+++ qml/utils/Utils.js
@@ -24,6 +24,27 @@ THE SOFTWARE.
 
 .pragma library
 
+function generateDateString(dt, format) {
+    var today = new Date()
+    var date = "";
+    if (today.getDate() === dt.getDate() &&
+            today.getMonth() === dt.getMonth() &&
+            today.getFullYear() === dt.getFullYear()) {
+        date = qsTr("Today") + ", "
+    }
+    else if (today.getDate() === dt.getDate() + 1 &&
+            today.getMonth() === dt.getMonth() &&
+            today.getFullYear() === dt.getFullYear()){
+        date = qsTr("Yesterday") + ", "
+    }
+
+    if(date !== "") {
+        return date + " " + Qt.formatTime(dt, "hh:mm")
+    }
+
+    return Qt.formatDateTime(dt, format)
+}
+
 function getDetailsInfoString(age, gender, location) {
     var result = ""
     if (age !== 0) {
