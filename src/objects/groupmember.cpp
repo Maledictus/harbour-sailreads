d5fe03b3b7002e91d88c9fe641aff2928bb24b5c
Oleg Linkin <MaledictusDeMagog@gmail.com>
2018-03-21 01:06:44 +0300

    Add groupmember object

---
+++ src/objects/groupmember.cpp
@@ -0,0 +1,61 @@
+/*
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "groupmember.h"
+
+namespace Sailreads
+{
+GroupMember::GroupMember()
+{
+
+}
+
+User GroupMember::GetUser() const
+{
+    return m_User;
+}
+
+void GroupMember::SetUser(const User& user)
+{
+    m_User = user;
+}
+
+QString GroupMember::GetTitle() const
+{
+    return m_Title;
+}
+
+void GroupMember::SetTitle(const QString& title)
+{
+    m_Title = title;
+}
+
+quint64 GroupMember::GetCommentsCount() const
+{
+    return m_CommentsCount;
+}
+
+void GroupMember::SetCommentsCount(quint64 count)
+{
+    m_CommentsCount = count;
+}
+} // namespace Sailreads
---
+++ src/objects/groupmember.h
@@ -0,0 +1,48 @@
+/*
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <QList>
+#include "../types.h"
+
+namespace Sailreads {
+
+class GroupMember
+{
+    User m_User;
+    QString m_Title;
+    quint64 m_CommentsCount;
+public:
+    GroupMember();
+
+    User GetUser() const;
+    void SetUser(const User& user);
+    QString GetTitle() const;
+    void SetTitle(const QString& title);
+    quint64 GetCommentsCount() const;
+    void SetCommentsCount(quint64 count);
+};
+
+typedef QList<GroupMember> GroupMembers_t;
+
+} // namespace Sailreads
