78e5859e00bd2781d90a05357be352630e94f613
Oleg Linkin <MaledictusDeMagog@gmail.com>
2018-03-21 01:06:53 +0300

    Add groupfolder object

---
+++ src/objects/groupfolder.cpp
@@ -0,0 +1,81 @@
+/*
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "groupfolder.h"
+
+namespace Sailreads
+{
+GroupFolder::GroupFolder()
+: m_Id(0)
+{
+}
+
+quint64 GroupFolder::GetId() const
+{
+    return m_Id;
+}
+
+void GroupFolder::SetId(quint64 id)
+{
+    m_Id = id;
+}
+
+QString GroupFolder::GetName() const
+{
+    return m_Name;
+}
+
+void GroupFolder::SetName(const QString& name)
+{
+    m_Name = name;
+}
+
+quint64 GroupFolder::GetItemsCount() const
+{
+    return m_ItemsCount;
+}
+
+void GroupFolder::SetItemsCount(quint64 count)
+{
+    m_ItemsCount = count;
+}
+
+quint64 GroupFolder::GetSubItemsCount() const
+{
+    return m_SubItemsCount;
+}
+
+void GroupFolder::SetSubItemsCount(quint64 count)
+{
+    m_SubItemsCount = count;
+}
+
+QDateTime GroupFolder::GetUpdatedAt() const
+{
+    return m_UpdatedAt;
+}
+
+void GroupFolder::SetUpdatedAt(const QDateTime& dt)
+{
+    m_UpdatedAt = dt;
+}
+} // namespace Sailreads
---
+++ src/objects/groupfolder.h
@@ -0,0 +1,53 @@
+/*
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <QList>
+#include <QDateTime>
+
+namespace Sailreads
+{
+class GroupFolder
+{
+    quint64 m_Id;
+    QString m_Name;
+    quint64 m_ItemsCount;
+    quint64 m_SubItemsCount;
+    QDateTime m_UpdatedAt;
+public:
+    GroupFolder();
+
+    quint64 GetId() const;
+    void SetId(quint64 id);
+    QString GetName() const;
+    void SetName(const QString& name);
+    quint64 GetItemsCount() const;
+    void SetItemsCount(quint64 count);
+    quint64 GetSubItemsCount() const;
+    void SetSubItemsCount(quint64 count);
+    QDateTime GetUpdatedAt() const;
+    void SetUpdatedAt(const QDateTime& dt);
+};
+
+typedef QList<GroupFolder> GroupFolders_t;
+} // namespace Sailreads
