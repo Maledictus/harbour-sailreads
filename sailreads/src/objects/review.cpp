fc4ab54eaf32aefb182b80a66732afaebfe20835
Oleg Linkin <MaledictusDeMagog@gmail.com>
2018-04-20 01:02:55 +0300

    Add Review object

---
+++ sailreads/src/objects/review.cpp
@@ -0,0 +1,185 @@
+/*
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "review.h"
+
+namespace Sailreads
+{
+Review::Review()
+: m_Id(0)
+, m_Rating(0)
+, m_Votes(0)
+, m_ReadCount(0)
+, m_CommentsCount(0)
+, m_Owned(0)
+{
+}
+
+quint64 Review::GetId() const
+{
+    return m_Id;
+}
+
+void Review::SetId(quint64 id)
+{
+    m_Id = id;
+}
+
+Book Review::GetBook() const
+{
+    return m_Book;
+}
+
+void Review::SetBook(const Book& book)
+{
+    m_Book = book;
+}
+
+int Review::GetRating() const
+{
+    return m_Rating;
+}
+
+void Review::SetRating(int rating)
+{
+    m_Rating = rating;
+}
+
+int Review::GetVotes() const
+{
+    return m_Votes;
+}
+
+void Review::SetVotes(int votes)
+{
+    m_Votes = votes;
+}
+
+BookShelves_t Review::GetShelves() const
+{
+    return m_Shelves;
+}
+
+void Review::SetShelves(const BookShelves_t& shelves)
+{
+    m_Shelves = shelves;
+}
+
+QStringList Review::GetShelvesList() const
+{
+    QStringList list;
+    std::transform(m_Shelves.begin(), m_Shelves.end(),
+        std::back_inserter(list),
+        [](decltype(m_Shelves.front()) shelf){ return shelf.GetName(); });
+    return list;
+}
+
+QDateTime Review::GetAddedDate() const
+{
+    return m_AddedDate;
+}
+
+void Review::SetAddedDate(const QDateTime& addedDate)
+{
+    m_AddedDate = addedDate;
+}
+
+QDateTime Review::GetUpdatedDate() const
+{
+    return m_UpdatedDate;
+}
+
+void Review::SetUpdatedDate(const QDateTime& updatedDate)
+{
+    m_UpdatedDate = updatedDate;
+}
+
+QDateTime Review::GetReadDate() const
+{
+    return m_ReadDate;
+}
+
+void Review::SetReadDate(const QDateTime& readDate)
+{
+    m_ReadDate = readDate;
+}
+
+QDateTime Review::GetStartedDate() const
+{
+    return m_StartedDate;
+}
+
+void Review::SetStartedDate(const QDateTime& startedDate)
+{
+    m_StartedDate = startedDate;
+}
+
+int Review::GetReadCount() const
+{
+    return m_ReadCount;
+}
+
+void Review::SetReadCount(int readCount)
+{
+    m_ReadCount = readCount;
+}
+
+QString Review::GetBody() const
+{
+    return m_Body;
+}
+
+void Review::SetBody(const QString& body)
+{
+    m_Body = body;
+}
+
+quint64 Review::GetCommentsCount() const
+{
+    return m_CommentsCount;
+}
+
+void Review::SetCommentsCount(const quint64& commentsCount)
+{
+    m_CommentsCount = commentsCount;
+}
+
+quint64 Review::GetOwned() const
+{
+    return m_Owned;
+}
+
+void Review::SetOwned(const quint64& owned)
+{
+    m_Owned = owned;
+}
+
+QUrl Review::GetUrl() const
+{
+    return m_Url;
+}
+
+void Review::SetUrl(const QUrl& url)
+{
+    m_Url = url;
+}
+} // namespace Sailreads
---
+++ sailreads/src/objects/review.h
@@ -0,0 +1,87 @@
+/*
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <QDateTime>
+#include <QString>
+
+#include "book.h"
+#include "bookshelf.h"
+
+namespace Sailreads
+{
+class Review
+{
+    Q_GADGET
+
+    quint64 m_Id;
+    Book m_Book;
+    int m_Rating;
+    int m_Votes;
+    BookShelves_t m_Shelves;
+    QDateTime m_AddedDate;
+    QDateTime m_UpdatedDate;
+    QDateTime m_ReadDate;
+    QDateTime m_StartedDate;
+    int m_ReadCount;
+    QString m_Body;
+    quint64 m_CommentsCount;
+    quint64 m_Owned;
+    QUrl m_Url;
+
+public:
+    Review();
+
+    quint64 GetId() const;
+    void SetId(quint64 id);
+    Book GetBook() const;
+    void SetBook(const Book& book);
+    int GetRating() const;
+    void SetRating(int rating);
+    int GetVotes() const;
+    void SetVotes(int votes);
+    BookShelves_t GetShelves() const;
+    void SetShelves(const BookShelves_t& shelves);
+    QStringList GetShelvesList() const;
+    QDateTime GetAddedDate() const;
+    void SetAddedDate(const QDateTime& addedDate);
+    QDateTime GetUpdatedDate() const;
+    void SetUpdatedDate(const QDateTime& updatedDate);
+    QDateTime GetReadDate() const;
+    void SetReadDate(const QDateTime& readDate);
+    QDateTime GetStartedDate() const;
+    void SetStartedDate(const QDateTime& startedDate);
+    int GetReadCount() const;
+    void SetReadCount(int readCount);
+    QString GetBody() const;
+    void SetBody(const QString& body);
+    quint64 GetCommentsCount() const;
+    void SetCommentsCount(const quint64& commentsCount);
+    quint64 GetOwned() const;
+    void SetOwned(const quint64& owned);
+    QUrl GetUrl() const;
+    void SetUrl(const QUrl& url);
+};
+
+typedef QList<Review> Reviews_t;
+} // namespace Sailreads
