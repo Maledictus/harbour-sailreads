7068aa5fa8e3b2bfb92b39076e415edbbde83572
Oleg Linkin <MaledictusDeMagog@gmail.com>
2018-05-05 23:33:14 +0300

    Move userprofile to qmltypes dir

---
+++ sailreads/src/qmltypes/userprofile.cpp
@@ -0,0 +1,82 @@
+﻿/*
+The MIT License(MIT)
+
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "userprofile.h"
+
+#include <QtDebug>
+
+#include "../models/bookshelvesmodel.h"
+#include "../objects/user.h"
+#include "../sailreadsmanager.h"
+
+namespace Sailreads
+{
+UserProfile::UserProfile(QObject *parent)
+: QObject(parent)
+, m_UserId(0)
+{
+    connect(SailreadsManager::Instance(), &SailreadsManager::gotUserProfile,
+            this, &UserProfile::handleGotUser);
+}
+
+quint64 UserProfile::GetUserID() const
+{
+    return m_UserId;
+}
+
+void UserProfile::SetUserID(quint64 id)
+{
+    if (m_UserId != id) {
+        m_UserId = id;
+        updateProfile();
+        emit userIdChanged();
+    }
+}
+
+User* UserProfile::GetUser() const
+{
+    return m_User.get();
+}
+
+void UserProfile::SetUser(const UserPtr& user)
+{
+    if (m_User != user) {
+        m_User = user;
+        return userChanged();
+    }
+}
+
+void UserProfile::handleGotUser(const UserPtr& user)
+{
+    if (!user || user->GetId() != m_UserId) {
+        return;
+    }
+    SetUser(user);
+}
+
+void UserProfile::updateProfile()
+{
+    SailreadsManager::Instance()->getUserInfo(m_UserId);
+}
+} // namespace Sailreads
---
+++ sailreads/src/qmltypes/userprofile.h
@@ -0,0 +1,68 @@
+﻿/*
+The MIT License(MIT)
+
+Copyright (c) 2018 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <memory>
+
+#include <QObject>
+#include <QString>
+#include <QVariantMap>
+#include <QUrl>
+
+#include "../types.h"
+
+namespace Sailreads
+{
+class BookShelvesModel;
+class User;
+
+class UserProfile : public QObject
+{
+    Q_OBJECT
+
+    quint64 m_UserId;
+    UserPtr m_User;
+
+    Q_PROPERTY(quint64 userId READ GetUserID WRITE SetUserID NOTIFY userIdChanged)
+    Q_PROPERTY(User* user READ GetUser NOTIFY userChanged)
+
+public:
+    explicit UserProfile(QObject *parent = nullptr);
+
+    quint64 GetUserID() const;
+    void SetUserID(quint64 id);
+    User* GetUser() const;
+    void SetUser(const UserPtr& user);
+
+private slots:
+    void handleGotUser(const UserPtr& user);
+public slots:
+    void updateProfile();
+
+signals:
+    void userIdChanged();
+    void userChanged();
+};
+} // namespace Sailreads
